5d3a104f144a250db73cfe8e2e185a317032eec3
KTarun003 <tarunreddykreddy@gmail.com>
2020-10-06 21:34:12 +0530

    Update Startup.cs

--- InterScOM/Startup.cs
+++ InterScOM/Startup.cs
@@ -29,7 +29,7 @@ namespace InterScOM
         {
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
+                    Configuration.GetConnectionString("AwsRds")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
