038abf283b2f0957a8682aca971a7714e562335c
KTarun003 <tarunreddykreddy@gmail.com>
2020-10-07 18:28:25 +0530

    Changed Index Page According to Needs

--- InterScOM/Areas/Forum/Models/Query.cs
+++ InterScOM/Areas/Forum/Models/Query.cs
@@ -16,6 +16,9 @@ namespace InterScOM.Areas.Forum.Models
 
         [Key]
         public int Id { get; set; }
+
+        public string Topic { get; set; }
+
         public string UserName { get; set; }
 
         public string Question { get; set; }
--- InterScOM/Migrations/20201007124829_UpdateQueryModel.Designer.cs
+++ InterScOM/Migrations/20201007124829_UpdateQueryModel.Designer.cs
@@ -3,14 +3,16 @@ using InterScOM.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace InterScOM.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201007124829_UpdateQueryModel")]
+    partial class UpdateQueryModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
