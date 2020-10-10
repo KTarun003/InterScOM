9f5550e1288d57c16478fdb23c7b5c4ac44d9e3d
KTarun003 <tarunreddykreddy@gmail.com>
2020-10-10 23:38:54 +0530

    Added Login Functionality

--- InterScOM/Startup.cs
+++ InterScOM/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using InterScOM.Areas.Admin.Data;
+using InterScOM.Areas.Admin.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI;
@@ -30,19 +32,29 @@ namespace InterScOM
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("AwsRds")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+            services.AddIdentity<AppUser, AppRole>(options => options.User.RequireUniqueEmail = true)
+                .AddEntityFrameworkStores<IdentityContext>();
+            services.AddDbContext<IdentityContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("AwsRds")));
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider serviceProvider)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseDatabaseErrorPage();
+                var roleManager = serviceProvider.GetRequiredService<RoleManager<AppRole>>();
+                var userManager = serviceProvider.GetRequiredService<UserManager<AppUser>>();
+                if (roleManager.FindByNameAsync("admin") == null &&
+                    userManager.FindByEmailAsync("Test@admin.com") == null)
+                {
+                    SeedData(serviceProvider).Wait();
+                }
             }
             else
             {
@@ -57,7 +69,7 @@ namespace InterScOM
 
             app.UseAuthentication();
             app.UseAuthorization();
-
+            
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
@@ -69,6 +81,52 @@ namespace InterScOM
                     pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
+        }
+
+        private async Task SeedData(IServiceProvider serviceProvider)
+        {
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<AppRole>>();
+            var userManager = serviceProvider.GetRequiredService<UserManager<AppUser>>();
+            AppUser adminUser = new AppUser
+            {
+                FirstName = "Test",
+                LastName = "Admin",
+                UserName = "Admin",
+                Email = "admin@Test.com"
+            };
+            AppUser staffUser = new AppUser
+            {
+                FirstName = "Test",
+                LastName = "Staff",
+                UserName = "Staff",
+                Email = "staff@Test.com"
+            };
+            AppUser parentUser = new AppUser
+            {
+                FirstName = "Test",
+                LastName = "Parent",
+                UserName = "Parent",
+                Email = "parent@Test.com"
+            };
+            await userManager.CreateAsync(adminUser, "Test@admin1");
+            await userManager.CreateAsync(staffUser, "Test@staff1");
+            await userManager.CreateAsync(parentUser, "Test@parent1");
+
+            AppRole adminRole = new AppRole()
+            {
+                Name = "admin"
+            };
+            AppRole staffRole = new AppRole()
+            {
+                Name = "staff"
+            };
+            AppRole parentRole = new AppRole()
+            {
+                Name = "parent"
+            };
+            await roleManager.CreateAsync(adminRole);
+            await roleManager.CreateAsync(staffRole);
+            await roleManager.CreateAsync(parentRole);
         }
     }
 }
